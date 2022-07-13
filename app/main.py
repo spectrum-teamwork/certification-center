e9094794dedfca38b421d4560fe3e472ef55cc26
YarLikViD <yarlistratenko@yandex.ru>
2022-07-13 17:27:41 +0300

    add: migrations

--- app/main.py
+++ app/main.py
@@ -10,9 +10,3 @@ from app.models import Base
 
 app = FastAPI() if settings.debug is True else FastAPI(docs_url=None, redoc_url=None)
 app.include_router(router)
-
-
-@app.on_event("startup")
-async def startup():
-    async with engine.begin() as conn: # TODO: Временное решение до миграций   
-        await conn.run_sync(Base.metadata.create_all)
