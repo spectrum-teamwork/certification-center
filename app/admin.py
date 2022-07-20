f7b5ab0b7147c681c0266334ac7c30ee1c3942f2
YarLikViD <yarlistratenko@yandex.ru>
2022-07-20 16:04:38 +0300

    feat: add ne admin models

--- app/admin.py
+++ app/admin.py
@@ -3,6 +3,7 @@ from sqladmin import ModelAdmin
 from sqladmin import widgets
 import wtforms
 
+
 class NewsAdmin(ModelAdmin, model=m.News):
     column_list = [m.News.title, m.News.text, m.News.image_id]
 
@@ -13,9 +14,33 @@ class ServiceAdmin(ModelAdmin, model=m.Service):
     column_labels = {m.Service.title: "Оглавление"}
 
     # Creating
-    form_columns = [m.Service.title, m.Service.service_type, m.Service.description, m.Service.price, m.Service.image_id, m.Service.image_document_id]
+    form_columns = [m.Service.title, m.Service.service_type, m.Service.description,
+                    m.Service.price, m.Service.image_id, m.Service.image_document_id]
     form_args = {'title': {'label': 'Оглавление'}}
     form_widget_args = {'price': {'model': widgets.TimePickerWidget}}
     from_overrides = {m.Service.image_id: wtforms.FileField}
 
     can_export = False
+
+
+class ContactAdmin(ModelAdmin, model=m.Contact):
+    column_list = [m.Contact.id, m.Contact.city, m.Contact.address]
+
+
+class ClientAdmin(ModelAdmin, model=m.Client):
+    column_list = [m.Client.id, m.Client.title]
+
+
+class CertificateAdmin(ModelAdmin, model=m.Certificate):
+    column_list = [m.Certificate.id, m.Certificate.label]
+
+
+class AccreditationInfoAdmin(ModelAdmin, model=m.AccreditationInfo):
+    column_list = [m.AccreditationInfo.id,
+                   m.AccreditationInfo.title, m.AccreditationInfo.text]
+
+    can_create = False
+
+
+class ImagesAdmin(ModelAdmin, model=m.Image):
+    column_list = [m.Image.id, m.Image.name]
